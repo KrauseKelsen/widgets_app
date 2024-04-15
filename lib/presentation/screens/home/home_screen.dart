293b159fe7739326e7f011bc13d29dbb5cbaa6ff
KrauseKelsen <kkelsen09@gmail.com>
2024-04-14 18:59:34 -0600

    buttons_screen finished

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
-import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -67,7 +67,7 @@ class CustomListTitle extends StatelessWidget {
         //);
         //Navigator.pushNamed(context, menuItem.link);
         //context.push(menuItem.link);
-        context.pushNamed(CardsScreen.name);
+        context.pushNamed(ButtonsScreen.name);
       },
     );
   }
